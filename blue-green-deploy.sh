e4d2f946b5fe695d234241f785d6c658941e7c05
hacckerank <me@hackerrank.com>
2021-09-01 17:01:12 +0530

    Update

--- blue-green-deploy.sh
+++ blue-green-deploy.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-​
 DEPLOYMENT_FILE="k8s/deployment.yaml"
 DEPLOYMENT_NAME="nodejs-deployment"
-BLUE_DEPLOYEMNET="$(kubectl get deploy -l app=coda-test-task -o jsonpath="{.items[0].metadata.name}")"
 GREEN_VERSION="$(git rev-parse --short HEAD)"
 DEPLOYMENT_REF="deploy/nodejs-deployment"
 REMOVE_PREVIOUS_DEPLOYMENT=true
 SERVICE_FILE="k8s/service.yaml"
+
+echo "checking this"
+
+BLUE_DEPLOYEMENT="$(kubectl get deploy -l app=coda-test-task -o jsonpath="{.items[0].metadata.name}")"
 ​
+echo "${BLUE_DEPLOYEMENT}"
+
 sed -i "s/VERSION_TO_REPLACE/${GREEN_VERSION}/g" ${DEPLOYMENT_FILE}
 sed -i "s/VERSION_TO_REPLACE/${GREEN_VERSION}/g" ${SERVICE_FILE}
 ​
